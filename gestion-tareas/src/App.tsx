edff8c4ea30f2e7a4d85a4cb1b06e4eac8028681
Jason Madrid <jasonhnn6@gmail.com>
2025-11-06 20:51:27 -0600

    verificando funcionamiento rama develop

--- gestion-tareas/src/App.tsx
+++ gestion-tareas/src/App.tsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-blue-700 p-4 m-2 text-white text-center text-4xl'>Hola</h1>
+      <h1 className='bg-blue-700 p-4 m-2 text-white text-center text-4xl'>Hola Mundo</h1>
     </>
   )
 }
